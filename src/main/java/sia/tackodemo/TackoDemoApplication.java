be98818cdd3918f4665a2b5e30873a6f1b523abf
borovyar <borovyar@!cvut.cz>
2022-07-28 13:56:34 +0200

    Code of the 1st chapter of "Spring in Action"

---
+++ src/main/java/sia/tackodemo/TackoDemoApplication.java
@@ -0,0 +1,13 @@
+package sia.tackodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TackoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TackoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sia/tackodemo/TackoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tackodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TackoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
